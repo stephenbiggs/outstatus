59ea5c0deb5f9bf23c9446beab0c16057e12c5a0
Stephen Biggs <stephen.biggs@hotmail.com>
2019-04-29 13:00:22 +0100

    Added missing email confirmation code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,20 @@ class UsersController < ApplicationController
   end
 
 
+  def confirm_email
+    user = User.find_by_confirm_token(params[:id])
+    if user
+      user.email_activate
+      flash[:success] = "Welcome to the Sample App! Your email has been confirmed.
+      Please sign in to continue."
+      redirect_to signin_url
+    else
+      flash[:error] = "Sorry. User does not exist"
+      redirect_to root_url
+    end
+  end
+
+
   def release_space
     @taken_space = Carspace.find_by_taken_user_id(current_user.id)
     @taken_space.taken_user_id = nil
